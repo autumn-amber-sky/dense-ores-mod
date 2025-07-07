c216af369c35ddbef849e080562ef3e6d28e3abe
AmberAutumnsky <SierraR@pm.me>
2025-07-07 14:56:07 +1200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dense Ores Mod
+A little project by Autumn and Leo.
+- Creates dense ore veins that cannot be mined with pickaxes.
+- Instead, they muse be excavated using create machinery, incentivising their usage.
---
+++ src/main/java/net/autumn/dense_ore/DenseOreMod.java
@@ -0,0 +1,49 @@
+package net.autumn.dense_ore;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DenseOreMod.MOD_ID)
+public class DenseOreMod
+{
+    public static final String MOD_ID = "dense_ore";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DenseOreMod(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
